e221b6dd1a0a6f486a39d7872de73edfddccce51
Sagar Saji <sagarsaji7@gmail.com>
2023-03-11 14:15:38 +0530

    third

--- src/test/java/ustbatchno3/junit5testcases/Vehicletest.java
+++ src/test/java/ustbatchno3/junit5testcases/Vehicletest.java
@@ -1,12 +1,16 @@
 package ustbatchno3.junit5testcases;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class Vehicletest {
 	
 	@Test
 	public void vehicaltest() {
-		String
-		
+		//String state = "KL";
+		int dis = 7;
+		String expected = "Ernakulam";
+		assertEquals(expected,Vehiche.district(dis));
 	}
 }
